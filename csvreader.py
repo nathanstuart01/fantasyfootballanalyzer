ba5fe3681dfc925085aeb3416ec5c924d9d43ebb
Nathan Stuart <nathan.stuart01@gmail.com>
2017-11-10 14:50:39 -0700

    added csv reader funciton

--- csvreader.py
+++ csvreader.py
@@ -1,5 +1,15 @@
 import csv
-with open('Fantasy Football Data Week 3 - Sheet1.csv') as f:
-    reader = csv.reader(f)
+
+def csv_reader(file_obj):
+    reader = csv.reader(file_obj)
+    results = []
     for row in reader:
-        print(row)
+        results.append(row)
+        results
+
+
+
+csv_path = "Fantasy Football Data Week 1 - Sheet1.csv"
+
+with open(csv_path, "r+") as f_obj:
+    csv_reader(f_obj)
